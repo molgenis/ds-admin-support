2f06603de1ef33373f3df769e8277c6cc84ea30f
Stuart Wheater <stuart.wheater@arjuna.com>
2024-11-01 15:29:24 +0000

    Generated string-boot skeleton

---
+++ study_update-webserver/src/main/java/org/molgenis/dsadmin/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package org.molgenis.dsadmin.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
---
+++ study_update-webserver/src/test/java/org/molgenis/dsadmin/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.molgenis.dsadmin.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
